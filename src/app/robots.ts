96cb3fee257ca210549f8505e68d0a42fa497a46
GrafikJAM <jekabs@mucenieks.lv>
2026-09-06 19:33:30 +0300

    Add '/login' to disallow list in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin', '/profile', '/success'],
+        disallow: ['/api/', '/admin', '/profile', '/success', '/login'],
       },
     ],
     sitemap: 'https://grafikjam.shop/sitemap.xml',
