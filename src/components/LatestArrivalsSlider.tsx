43dd76c9645aa8cd6c169dcdae1c6d219df1b08b
GrafikJAM <jekabs@mucenieks.lv>
2026-09-11 13:25:35 +0300

    Remove 'uniform' prop from ProductGrid component

--- src/components/LatestArrivalsSlider.tsx
+++ src/components/LatestArrivalsSlider.tsx
@@ -54,7 +54,7 @@ export default function LatestArrivalsSlider({ products }: { products: Product[]
         >
           {chunks.map((chunk, i) => (
             <div key={i} className={styles.slide} style={{ width: `${100 / chunks.length}%` }}>
-                            <ProductGrid products={chunk} cols={4} uniform />
+                             <ProductGrid products={chunk} cols={4} />
             </div>
           ))}
         </div>
